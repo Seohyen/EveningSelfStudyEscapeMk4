263b83baa7ed1c8e1428f7bdfd22797917622190
SeoHyun <namja4097@gmail.com>
2022-12-21 20:27:06 +0900

    merge

--- Assets/1.Scripts/Inventoryrelated/CreateItem.cs
+++ Assets/1.Scripts/Inventoryrelated/CreateItem.cs
@@ -5,11 +5,60 @@ using UnityEngine;
 public class CreateItem : MonoBehaviour
 {
     [SerializeField]
-    List<GameObject> item1;
+    private GameObject[] ItemObj1;
+    [SerializeField]
+    private GameObject[] ItemObj2;
+    [SerializeField]
+    private GameObject[] ItemObj3;
+    [SerializeField]
+    private GameObject[] ItemObj4;
 
+
+    private void Start()
+    {
+        CreateItem1();
+        CreateItem2();
+        CreateItem3();
+        CreateItem4();
+    }
 
     private void CreateItem1()
     {
-        
+         int selection = Random.Range(0, ItemObj1.Length);
+        GameObject selectedPrefab = ItemObj1[selection];
+
+        for(int i =0; i< Random.Range(1, 3); i++)
+        {
+        selectedPrefab.SetActive(true);
+        }
+    }
+    private void CreateItem2()
+    {
+        int selection = Random.Range(0, ItemObj2.Length);
+        GameObject selectedPrefab = ItemObj2[selection];
+
+        for (int i = 0; i < Random.Range(1, 3); i++)
+        {
+            selectedPrefab.SetActive(true);
+        }
+    }
+
+    private void CreateItem3()
+    {
+        int selection = Random.Range(0, ItemObj3.Length);
+        GameObject selectedPrefab = ItemObj3[selection];
+
+        for (int i = 0; i < Random.Range(1, 3); i++)
+        {
+            selectedPrefab.SetActive(true);
+        }
+    }
+
+    private void CreateItem4()
+    {
+        int selection = Random.Range(0, ItemObj4.Length);
+        GameObject selectedPrefab = ItemObj1[selection];
+
+            selectedPrefab.SetActive(true);
     }
 }
